71afb4e4d4ff40feb517a9e6ecfe33fb31308298
leonel-feverup <leonel.meque@feverup.com>
2025-03-02 16:39:00 +0100

    init commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient(withInterceptors([]))],
+}).catch((err) => console.error(err));
